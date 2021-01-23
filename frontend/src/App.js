32e8d5856604a2e63dbca555ff1e487b78208ce4
Rituraj Borpujari <riturajborpujari@outlook.com>
2021-01-23 19:13:35 +0530

    renamed pages to containers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import store from './store';
-import Homepage from './pages/Homepage';
+import Homepage from './containers/Homepage';
 import PrivateRoute from './components/general/PrivateRoute';
 import SigninForm from './components/Auth/Signin';
 
 function App() {
-
   return (
     <Provider store={store}>
       <Router>
