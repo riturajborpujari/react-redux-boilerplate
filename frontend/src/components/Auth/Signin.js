be4e4173a183b8e53f182e1214f23dcfe05bb9cc
Rituraj Borpujari <riturajborpujari@outlook.com>
2021-01-23 18:25:12 +0530

    used fake form for signin

--- frontend/src/components/Auth/Signin.js
+++ frontend/src/components/Auth/Signin.js
@@ -1,22 +1,109 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { Formik } from 'formik';
+
 import * as AuthActions from '../../store/actions/Auth';
 import Auth from '../../services/Auth';
+import Button from '../styled/Button';
 
-const Signin = () => {
+const SigninForm = () => {
   const dispatch = useDispatch();
-  const [isSubmitting, setSubmitting] = useState(false);
+  const { isSignedIn } = useSelector(state => state.auth);
 
-  const onSubmit = () => {
-    setSubmitting(true);
-    
-    Auth.signin(user => {
-      dispatch(AuthActions.SignIn(user));
-      setSubmitting(false);
-    })
-  }
+  /*if user is already authenticated */
+  if (isSignedIn) return <Redirect to='/' />;
 
-  return <button onClick={onSubmit} disabled={isSubmitting}>Sign In</button>;
+  return (
+    <div className="auth-wrapper">
+      <Formik
+        initialValues={{
+          email: "",
+          password: ""
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          Auth.signin(values, user => {
+            if (user) {
+              /** Auth succeeded */
+              dispatch(AuthActions.SignIn(user));
+            }
+            else {
+              /** 
+               * Auth failed
+               * @todo SHOW error message 
+               */
+            }
+            setSubmitting(false);
+          })
+        }}
+
+        validate={values => {
+          let errors = {};
+
+          if (values.password.length < 8) {
+            errors.password = "Password must be of 8 characters"
+          }
+          return errors;
+        }}
+      >
+        {({
+            values,
+            touched,
+            errors,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            handleSubmit
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <h2>Login</h2>
+
+              <div className="form-group">
+                <label>Email</label>
+                <input
+                  name="email"
+                  type="text"
+                  required
+                  className="form-control"
+                  placeholder="Email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </div>
+              {errors.email && touched.email && (
+                <div className="auth-feedback">{errors.email}</div>
+              )}
+
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  required
+                  className="form-control"
+                  placeholder="Password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </div>
+              {errors.password && touched.password && (
+                <div className="auth-feedback">{errors.password}</div>
+              )}
+
+              <Button
+                onClick={handleSubmit}
+                bg="#28D"
+                color="#FFF"
+                disabled={isSubmitting}
+                rounded
+              >Sign In</Button>
+            </form>
+          )
+        }
+      </Formik>
+    </div>
+  );
 }
-
-export default Signin;
+export default SigninForm;
